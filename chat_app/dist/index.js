605f396229e13dea59bfff7e7901f80895c936a0
mani-s12g <mani.s12g@gmail.com>
2026-06-02 13:28:08 +0530

    added a context to chat...

--- chat_app/dist/index.js
+++ chat_app/dist/index.js
@@ -7,20 +7,42 @@ const client = new OpenAI({
     apiKey: OPENAI_API_KEY,
     baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
 });
+const context = [
+    { role: "system", content: 'You are a helpful assistant' },
+    // { role: "user", content: 'Hello' },
+];
+async function chatCompletion() {
+    const res = await client.chat.completions.create({
+        model: "gemini-2.5-flash",
+        messages: context,
+        max_tokens: 50
+    });
+    const resMessage = res?.choices[0]?.message?.content ?? "";
+    context.push({
+        role: "assistant",
+        content: resMessage,
+    });
+    console.log("Assistant: ", resMessage);
+}
 async function main() {
     const input = promptSync({ sigint: true });
     while (true) {
-        const userInput = input("Enter your message: ");
+        const userInput = input("User I/P: ");
         if (userInput.toLowerCase() === "exit") {
             console.log("Exiting the chat app. Goodbye!");
             break;
         }
-        const chatCompletion = await client.chat.completions.create({
-            model: "gemini-2.5-flash",
-            messages: [{ role: "user", content: userInput }],
-            max_tokens: 50
+        context.push({
+            role: 'user',
+            content: userInput
         });
-        console.log("chatCompletion:", chatCompletion?.choices[0]?.message.content);
+        await chatCompletion();
+        // const chatCompletion = await client.chat.completions.create({
+        //     model: "gemini-2.5-flash",
+        //     messages: [{ role: "user", content: userInput }],
+        //     max_tokens: 50
+        // });
+        // console.log("chatCompletion:", chatCompletion?.choices[0]?.message.content);
     }
 }
 main();
